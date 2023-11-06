995cb3443e6a88df4779db750d7fc8a2ad3cdd26
Julian Wong <e0725200@u.nus.edu>
2023-11-06 13:50:03 +0800

    working script to generate softmax

--- generate_probabilities.py
+++ generate_probabilities.py
@@ -1,6 +1,7 @@
 import os
 import re
 import numpy as np
+import pandas as pd
 
 def softmax(x):
     e_x = np.exp(x - np.max(x))
@@ -11,7 +12,10 @@ def extract_data(directory):
         files.sort()
         for i in range(0, len(files), 2):
             file1_path = os.path.join(root, files[i])
-            file2_path = os.path.join(root, files[i+1])
+            if i+1 < len(files):
+                file2_path = os.path.join(root, files[i+1])
+            else:
+                continue  # Skip the rest of the loop for the last file if there's no matching pair
 
             with open(file1_path, 'r') as f1, open(file2_path, 'r') as f2:
                 data1 = f1.read()
@@ -31,10 +35,19 @@ def extract_data(directory):
 
                     softmax_probabilities = softmax(np.array([average_probability1, average_probability2]))
 
-                    f.write(f'Match: {match1.replace("_away", "")}\n')
-                    f.write(f'Softmax Probability: {softmax_probabilities[0]}\n\n')
-                    f.write(f'Match: {match2.replace("_home", "")}\n')
-                    f.write(f'Softmax Probability: {softmax_probabilities[1]}\n\n')
+                    match1 = match1.replace('-', ' ')
+                    match2 = match2.replace('-', ' ')
+                    info_array = match1.split('_')
+                    year = info_array[0]
+                    team1 = info_array[2]
+                    team2 = info_array[3]
+                    csv_file = pd.read_csv(f'Datasets/matches/epl_matches_{year}.csv')
+                    match_row = csv_file[((csv_file['home_team'] == team1) & (csv_file['away_team'] == team2))]
+                    if not match_row.empty:
+                        match_url = match_row['match_url'].values[0]
 
+                        f.write(f'Match: {match1.replace("_away", "").replace("_home", "")}\n')
+                        f.write(f'Softmax Probability: {softmax_probabilities[0]}\n')
+                        f.write(f'Match Url: {match_url}\n\n')
 # Call the function with your directory path
 extract_data('probability_results')
