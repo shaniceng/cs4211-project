8b9291cca93b52b491b9042874394291b41b1f48
shaniceng <ngshanice1@yahoo.com.sg>
2023-11-27 21:16:43 +0800

    push run_commands

---
+++ run_commands.bat
@@ -0,0 +1,14 @@
+@echo off
+
+PAT3.Console.exe -pcsp -d 20152016_away 20152016_away.txt
+PAT3.Console.exe -pcsp -d 20152016_home 20152016_home.txt
+PAT3.Console.exe -pcsp -d 20162017_away 20162017_away.txt
+PAT3.Console.exe -pcsp -d 20162017_home 20162017_home.txt
+PAT3.Console.exe -pcsp -d 20172018_away 20172018_away.txt
+PAT3.Console.exe -pcsp -d 20172018_home 20172018_home.txt
+PAT3.Console.exe -pcsp -d 20182019_away 20182019_away.txt
+PAT3.Console.exe -pcsp -d 20182019_home 20182019_home.txt
+PAT3.Console.exe -pcsp -d 20192020_away 20192020_away.txt
+PAT3.Console.exe -pcsp -d 20192020_home 20192020_home.txt
+PAT3.Console.exe -pcsp -d 20202021_away 20202021_away.txt
+PAT3.Console.exe -pcsp -d 20202021_home 20202021_home.txt
